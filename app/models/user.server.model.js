50a8773271afa9e0f9886280b14910d2a795d92d
mmaia <maia.marcos@gmail.com>
2015-08-04 23:50:15 -0300

    finished local strategy authentication implementation

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var mongoose = require('mongoose'),
+    crypto = require('crypto'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -12,13 +13,13 @@ var UserSchema = new Schema({
     email: {
         type: String,
         index: true,
-        match: /.+\@.+\..+/
+        match: [/.+\@.+\..+/, "Please use a valid email address"]
     },
     username: {
         type: String,
         trim: true,
         unique: true,
-        index: true
+        required: 'Username is required, please fill'
     },
     password: {
         type: String,
@@ -28,38 +29,63 @@ var UserSchema = new Schema({
             }, 'Password should be longer, at least 6'
         ]
     },
+    salt: {
+        type: String
+    },
+    provider:{
+        type: String,
+        required: 'Provider is required'
+    },
+    providerId: String,
+    providerData: {},
     created: {
         type: Date,
         default: Date.now
-    },
-    website: {
-        type: String,
-        set: function(url){
-            if(!url){
-                return url;
-            }else{
-                if(url.indexOf('http://' !== 0) && url.indexOf('https://' !== 0)){
-                    url = 'http://' + url;
-                }
-                return url;
-            }
-        },
-        get: function(url){
-            if(!url){
-                return url;
-            }else{
-                if(url.indexOf('http://' !== 0) && url.indexOf('https://' !== 0)){
-                    url = 'http://' + url;
-                }
-                return url;
-            }
-        }
     }
 });
 
 UserSchema.virtual('fullName').get(function(){
     return this.firstName + ' ' + this.lastName;
+}).set(function(fullName){
+    var splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
 });
+
+UserSchema.pre('save', function(next){
+    if(this.password){
+        this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.password = this.hashPassword(this.password);
+    }
+    next();
+});
+
+UserSchema.methods.hashPassword = function(password){
+    return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password){
+    return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback){
+    var _this = this;
+    var possibleUsername = username + (suffix || '');
+    _this.findOne({
+        username: possibleUsername
+    }, function(err, user){
+        if(!err){
+            if(!user){
+                callback(possibleUsername);
+            }else{
+                return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+            }
+        }else {
+            callback(null);
+        }
+    });
+};
+
 
 //registers the getters to be called any time the object is loaded from MongoDB
 UserSchema.set('toJSON', {getters: true, virtuals: true});
--- config/passport.js
+++ config/passport.js
@@ -2,3 +2,22 @@
  * Created by mmaia on 02/08/15.
  */
 'use strict';
+var passport = require('passport'),
+    mongoose = require('mongoose');
+
+module.exports = function () {
+    var User = mongoose.model('User');
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findOne({
+            _id: id
+        }, '-password -salt', function (err, user) {
+            done(err, user);
+        });
+    });
+    require('./strategies/local.js')();
+}
